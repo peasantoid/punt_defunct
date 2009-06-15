0c0022492777412abdbd1814bee1c7ef145a1d00
Peasantoid <peasantoid@archlinux.us>
2009-06-14 20:01:06 -0400

    completed bit

--- modules/bit/bit.c
+++ modules/bit/bit.c
@@ -21,13 +21,14 @@
 
 #include "../../src/common.h"
 #include "../../src/value.h"
+#include "../../src/pointer.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void checkargs(p_val *args, char *func, int minargs) {
-  if(val_llen(args) != 2) {
-    fprintf(stderr, "%s: %d args required\n", func, minargs);
+  if(val_llen(args) != minargs) {
+    fprintf(stderr, "%s: %d %s required\n", func, minargs, (minargs == 1) ? "arg" : "args");
     exit(1);
   }
 
@@ -44,11 +45,11 @@ char **_punt_list_funcs() {
   char **funcs = (char **)calloc(7, sizeof(char *));
 
   funcs[0] = "bitnot";
-  /*funcs[1] = "bitor";
+  funcs[1] = "bitor";
   funcs[2] = "bitand";
   funcs[3] = "bitxor";
   funcs[4] = "bitl";
-  funcs[5] = "bitr";*/
+  funcs[5] = "bitr";
 
   return funcs;
 }
@@ -57,6 +58,52 @@ p_val punt_bitnot(p_val *args, p_var **vars) {
   p_val rval = val_make();
 
   checkargs(args, "bitnot", 1);
+  rval.val = ptr_dupint(~*(long *)args[0].val);
+
+  return rval;
+}
+
+p_val punt_bitor(p_val *args, p_var **vars) {
+  p_val rval = val_make();
+
+  checkargs(args, "bitor", 2);
+  rval.val = ptr_dupint(*(long *)args[0].val | *(long *)args[1].val);
+
+  return rval;
+}
+
+p_val punt_bitand(p_val *args, p_var **vars) {
+  p_val rval = val_make();
+
+  checkargs(args, "bitand", 2);
+  rval.val = ptr_dupint(*(long *)args[0].val & *(long *)args[1].val);
+
+  return rval;
+}
+
+p_val punt_bitxor(p_val *args, p_var **vars) {
+  p_val rval = val_make();
+
+  checkargs(args, "bitxor", 2);
+  rval.val = ptr_dupint(*(long *)args[0].val ^ *(long *)args[1].val);
+
+  return rval;
+}
+
+p_val punt_bitl(p_val *args, p_var **vars) {
+  p_val rval = val_make();
+
+  checkargs(args, "bitl", 2);
+  rval.val = ptr_dupint(*(long *)args[0].val << *(long *)args[1].val);
+
+  return rval;
+}
+
+p_val punt_bitr(p_val *args, p_var **vars) {
+  p_val rval = val_make();
+
+  checkargs(args, "bitr", 2);
+  rval.val = ptr_dupint(*(long *)args[0].val >> *(long *)args[1].val);
 
   return rval;
 }
